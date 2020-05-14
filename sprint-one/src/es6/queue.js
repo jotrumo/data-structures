9bf481f127ee26702b423776b84302da2e6fa523
jotrumo <jtmontoya26@gmail.com>
2020-05-14 03:15:44 -0700

    ES6 exercises stack and queue completed. All tests passing.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,40 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.sizeObj = 0;
+    this.enqueueNumKey = 0;
+    this.dequeueNumKey = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.enqueueNumKey] = value;
+    this.enqueueNumKey++;
+    this.sizeObj++;
+  };
+
+  dequeue() {
+    if (this.sizeObj > 0) {
+      var result = this.storage[this.dequeueNumKey];
+      delete this.storage[this.dequeueNumKey];
+      this.dequeueNumKey++;
+      this.sizeObj--;
+    }
+    return result;
+  };
+
+  size() {
+    return this.sizeObj;
+  };
+
 }
+
+
+
+
+
+
+
+
+
+
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,37 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage ={};
+    this.keyNum = 0;
+    this.sizeObj = 0;
   }
 
+
+  push(value) {
+      this.storage[this.keyNum] = value;
+      this.keyNum++;
+      this.sizeObj++;
+  };
+
+  pop() {
+      if (this.sizeObj > 0) {
+        this.keyNum--;
+        var result = this.storage[this.keyNum];
+        delete this.storage[this.keyNum];
+        this.sizeObj--;
+      }
+      return result;
+  };
+
+  size() {
+      return this.sizeObj;
+  };
 }
+
+
+
+
+
+
+
+
