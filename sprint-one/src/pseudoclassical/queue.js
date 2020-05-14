ee6595f18549f07be6176e0bad41435cf9f85af1
jotrumo <jtmontoya26@gmail.com>
2020-05-14 03:08:33 -0700

    Completed pseudoclassical exercises for both queue and stack. All tests passing.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeObj = 0;
+  this.enqueueNumKey = 0;
+  this.dequeueNumKey = 0;
 };
+
+
+
+Queue.prototype.enqueue = function(value) {
+    this.storage[this.enqueueNumKey] = value;
+    this.enqueueNumKey++;
+    this.sizeObj++;
+};
+
+Queue.prototype.dequeue = function() {
+    if (this.sizeObj > 0) {
+      var result = this.storage[this.dequeueNumKey];
+      delete this.storage[this.dequeueNumKey];
+      this.dequeueNumKey++;
+      this.sizeObj--;
+    }
+    return result;
+};
+
+Queue.prototype.size = function() {
+    return this.sizeObj;
+};
+
+
+
+
+
+
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage ={};
+  this.keyNum = 0;
+  this.sizeObj = 0;
 };
+
+
+Stack.prototype.push = function(value) {
+    this.storage[this.keyNum] = value;
+    this.keyNum++;
+    this.sizeObj++;
+};
+
+Stack.prototype.pop = function() {
+    if (this.sizeObj > 0) {
+      this.keyNum--;
+      var result = this.storage[this.keyNum];
+      delete this.storage[this.keyNum];
+      this.sizeObj--;
+    }
+    return result;
+};
+
+Stack.prototype.size = function() {
+    return this.sizeObj;
+};
+
+
+
+
+
+
 
 
