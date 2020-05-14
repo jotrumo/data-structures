8d99bf3732ab5a57015f850b7ec1cfd38377c5ba
jotrumo <jtmontoya26@gmail.com>
2020-05-13 18:20:05 -0700

    Quick Save - completed functional stack exercise. All tests passing.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var keyNum = 0;
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[keyNum] = value;
+    keyNum++;
+    size++;
+
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      keyNum--;
+      size--;
+      var result = storage[keyNum];
+      delete storage[keyNum];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
