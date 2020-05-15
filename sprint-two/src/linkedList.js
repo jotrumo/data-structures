1834aa74e69c9e5ff93d8658699d5e6c09fc4c47
jotrumo <jtmontoya26@gmail.com>
2020-05-14 17:05:12 -0700

    Quick Save - linklist tests mostly passing. Still need to work through contains method.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,42 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  var previousNode = null;
+
   list.addToTail = function(value) {
+    var nodeObj = new Node(value);
+
+    //if list.head === null, do
+    if (list.head === null) {
+      //set nodeObj to list.head
+      list.head = nodeObj;
+      list.tail = nodeObj;
+      previousNode = nodeObj;
+      console.log("PreviousNode :", previousNode);
+
+    } else if (list.head) {
+
+      previousNode.next = nodeObj;
+      console.log('SecondNode: ', previousNode);
+      previousNode = nodeObj
+      list.tail = nodeObj;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      var result = list.head.value;
+      // console.log("result :", result);
+      list.head = list.head.next;
+    }
+
+    return result;
   };
 
   list.contains = function(target) {
-  };
 
+
+  }
   return list;
 };
 
@@ -23,6 +50,12 @@ var Node = function(value) {
 
   return node;
 };
+
+var test = LinkedList();
+
+test.addToTail(5);
+test.addToTail(6);
+console.log(test);
 
 /*
  * Complexity: What is the time complexity of the above functions?
