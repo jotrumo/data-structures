3b2da3b9d168b84f572fb5446dd9fae842dcfa29
jotrumo <jtmontoya26@gmail.com>
2020-05-15 15:08:59 -0700

    Completed set exercise - all tests passing

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if(Object.keys(this._storage).indexOf(item) > -1) {
+    return true;
+  } else{
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Answer: Constant time complexity - worse case scenerio.
  */
